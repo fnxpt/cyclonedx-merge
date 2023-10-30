9fcf73314b88038fd3f78ef55b24f3357aaa228c
Marlon Pina Tojal <marlont@backbase.com>
2023-10-30 12:44:32 +0100

    add some tests

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module cyclonedx-merge
 
 go 1.21.1
 
-require github.com/CycloneDX/cyclonedx-go v0.7.2
+require (
+	github.com/CycloneDX/cyclonedx-go v0.7.2
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ merge_test.go
@@ -0,0 +1,118 @@
+package cyclonedxmerge
+
+import (
+	"testing"
+
+	cyclonedx "github.com/CycloneDX/cyclonedx-go"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestMergeComponents
+func TestMergeComponents(t *testing.T) {
+	firstObject := []cyclonedx.Component{{
+		BOMRef:  "A:1",
+		Name:    "A",
+		Version: "1",
+	},
+		{
+			BOMRef:  "B:2",
+			Name:    "B",
+			Version: "2",
+		},
+	}
+	secondObject := []cyclonedx.Component{{
+		BOMRef:  "A:1",
+		Name:    "A",
+		Version: "1",
+	},
+		{
+			BOMRef:  "B:2",
+			Name:    "B",
+			Version: "2",
+		},
+	}
+
+	AddIfNew(&firstObject, &secondObject, "")
+	assert.Equal(t, 2, len(firstObject))
+	assert.Equal(t, "A:1", firstObject[0].BOMRef)
+	assert.Equal(t, "A", firstObject[0].Name)
+	assert.Equal(t, "1", firstObject[0].Version)
+	assert.Equal(t, "B:2", firstObject[1].BOMRef)
+	assert.Equal(t, "B", firstObject[1].Name)
+	assert.Equal(t, "2", firstObject[1].Version)
+}
+
+func TestMergeComponentsWithDifferentObjects(t *testing.T) {
+	firstObject := []cyclonedx.Component{{
+		BOMRef:  "A:1",
+		Name:    "A",
+		Version: "1",
+	},
+		{
+			BOMRef:  "B:2",
+			Name:    "B",
+			Version: "2",
+		},
+	}
+	secondObject := []cyclonedx.Component{{
+		BOMRef:  "A:1",
+		Name:    "A",
+		Version: "1",
+	},
+		{
+			BOMRef:  "B:2",
+			Name:    "B",
+			Version: "3",
+		},
+	}
+
+	AddIfNew(&firstObject, &secondObject, "")
+	assert.Equal(t, 2, len(firstObject))
+	assert.Equal(t, "A:1", firstObject[0].BOMRef)
+	assert.Equal(t, "A", firstObject[0].Name)
+	assert.Equal(t, "1", firstObject[0].Version)
+	assert.Equal(t, "B:2", firstObject[1].BOMRef)
+	assert.Equal(t, "B", firstObject[1].Name)
+	assert.Equal(t, "2", firstObject[1].Version)
+}
+
+// TestMergeComponentsNested
+func TestMergeComponentsNested(t *testing.T) {
+	firstObject := []cyclonedx.Component{{
+		BOMRef:  "A:1",
+		Name:    "A",
+		Version: "1",
+	},
+		{
+			BOMRef:  "B:2",
+			Name:    "B",
+			Version: "2",
+		},
+	}
+	secondObject := []cyclonedx.Component{{
+		BOMRef:  "A:1",
+		Name:    "A",
+		Version: "1",
+	},
+		{
+			BOMRef:  "B:2",
+			Name:    "B",
+			Version: "2",
+		},
+	}
+
+	AddIfNew(&firstObject, &secondObject, "X|")
+	assert.Equal(t, 4, len(firstObject))
+	assert.Equal(t, "A:1", firstObject[0].BOMRef)
+	assert.Equal(t, "A", firstObject[0].Name)
+	assert.Equal(t, "1", firstObject[0].Version)
+	assert.Equal(t, "B:2", firstObject[1].BOMRef)
+	assert.Equal(t, "B", firstObject[1].Name)
+	assert.Equal(t, "2", firstObject[1].Version)
+	assert.Equal(t, "X|A:1", firstObject[2].BOMRef)
+	assert.Equal(t, "A", firstObject[2].Name)
+	assert.Equal(t, "1", firstObject[2].Version)
+	assert.Equal(t, "X|B:2", firstObject[3].BOMRef)
+	assert.Equal(t, "B", firstObject[3].Name)
+	assert.Equal(t, "2", firstObject[3].Version)
+}
